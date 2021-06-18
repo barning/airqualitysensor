d6a22206920aef08b441104bb195b87648288982
Niklas Barning <nb@niklasbarning.de>
2021-06-18 15:24:54 +0200

    Adds a command to send a detailed report to Telegram after waking up.

--- airqualitysensor.ino
+++ airqualitysensor.ino
@@ -30,15 +30,16 @@ void setup()
   Serial.print("\nWiFi connected. IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.print("Retrieving time: ");
+  Serial.print("Retrieving time…\n");
   configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+
+  botSetup();
 
   if (airSensor.begin() == false)
   {
     Serial.println("Air sensor not detected. Please check wiring. Freezing...");
     bot.sendMessage(CHATID, "Air sensor not detected. Please check wiring.\n", "");
-    while (1)
-      ;
+    while (1);
   }
 
   Serial.print("Setup end\n");
@@ -47,16 +48,48 @@ void setup()
 void loop()
 {
   Serial.print("Begin Loop\n");
-  if ((airSensor.dataAvailable()) && (airSensor.getCO2() > 1000)) {
-    Serial.print("CO2 too high");
-    String message = translateActualValue + String(airSensor.getCO2()) + "ppm.\n" + translateOpen;
-    bot.sendMessage(CHATID, message, "");
-  } else {
-    Serial.print("Air Quality is Ok");
+  if ((airSensor.dataAvailable())) {
+    if (airSensor.getCO2() > 1000) {
+      Serial.print("CO2 too high\n");
+      String message = translateActualValue + String(airSensor.getCO2()) + "ppm.\n" + translateOpen;
+      bot.sendMessage(CHATID, message, "");
+    } else {
+      Serial.print("Air Quality is Ok\n");
     }
 
-  Serial.print("Begin Sleep\n");
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
-  ESP.deepSleep(time_between * 1000);
-  delay(100);
+    while (numNewMessages)
+    {
+      Serial.println("User wants something!\n");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    Serial.print("Begin Sleep\n");
+    WiFi.disconnect();
+    ESP.deepSleep(time_between * 1000);
+    delay(100);
+  }
+}
+
+void handleNewMessages(int numNewMessages)
+{
+  for (int i = 0; i < numNewMessages; i++) {
+    String text = bot.messages[i].text;
+    if (text == "/getStatus")
+    {
+      String message = "CO2: " + String(airSensor.getCO2()) + "ppm.\n" + "Temperature: " + airSensor.getTemperature() + "C\n" + "Humidity: " + airSensor.getHumidity() + "%";
+      bot.sendMessage(CHATID, message, "");
+    }
+  }
+}
+
+void botSetup()
+{
+  const String commands = F("["
+                            "{\"command\":\"start\", \"description\":\"Hello, from now on I will always inform you when it is time to open the window\"},"
+                            "{\"command\":\"getStatus\",\"description\":\"Responds with the sensor data the next time it wakes up\"}" // no comma on last command
+                            "]");
+  bot.setMyCommands(commands);
 }
